d29924b23f924ce970273c68326f7bbbd6b9ea46
Andrew Harkins <ExtremelyExceeding@gmail.com>
2019-04-01 00:07:55 -0500

    Add project files.

---
+++ MVC_TimeSh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_TimeSh.Startup))]
+namespace MVC_TimeSh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
